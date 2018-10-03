6d5d0d38392441272e0956de3cba35b162506dfe
Sub <sandedshoes@gmail.com>
2018-10-03 19:31:03 +0100

    Update Cube.java

--- src/net/spectre/Cube.java
+++ src/net/spectre/Cube.java
@@ -2,13 +2,13 @@ package net.spectre;
 
 public class Cube {
 	
-	public double posX = 0, posY = 0, posZ = 0;
-	public double offX = 0, offY = 0, offZ = 0;
-	public double rotX = 0, rotY = 0, rotZ = 0;
-	public int sizeX = 0, sizeY = 0, sizeZ = 0;
-	public int texU = 0, texV = 0;
-	public String name = "";
-	public boolean mirror = false;
+	public double posX = 0, posY = 0, posZ = 0; // Pos
+	public double offX = 0, offY = 0, offZ = 0; // Offset
+	public double rotX = 0, rotY = 0, rotZ = 0; // Rotation 
+	public int sizeX = 0, sizeY = 0, sizeZ = 0; // Shape
+	public int texU = 0, texV = 0; // Texture U/Vs
+	public String name = "NONAME"; // Shape name
+	public boolean mirror = false; //Where this shape has a mirror texture
 	
 	public Cube() {}
 	
@@ -45,8 +45,8 @@ public class Cube {
 		this.sizeZ = z;
 	}
 	
-	public void setMirror(boolean b) {
-		this.mirror = b;
+	public void setMirror(boolean mirrored) {
+		this.mirror = mirrored;
 	}
 
 }
